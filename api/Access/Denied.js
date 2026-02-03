f45fffc6222ad3cbee57c294f9e2a65e5a12021c
KioTheGreatV10 <rmpgsennn@gmail.com>
2026-02-03 11:29:54 +0800

    Delete api directory

--- api/Access/Denied.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.status(403).send(
-    "Access Denied\nContact LoneByte"
-  );
-}
--- api/Unauthorized/Access.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.status(401).send(
-    "Unauthorized Access\nContact LoneByte"
-  );
-}
